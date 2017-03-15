9f9363be5b581a1f1fcba892f20475ea47bae369
Nikolai Topping <nikolai.topping@gmail.com>
2017-03-15 10:52:23 +0100

    all

---
+++ File_Transfer/UPD/File_Client/File_Client.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace File_Client
+{
+	public class File_Client
+	{
+		public File_Client ()
+		{
+		}
+	}
+}
+
---
+++ File_Transfer/UPD/File_Server/File_Server.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace File_Server
+{
+	public class File_Server
+	{
+		private IPEndPoint _remoteIp;
+		private UdpClient _udpServer;
+		public File_Server (int portnumber)
+		{
+			_udpServer = new UdpClient (portnumber);
+			_remoteIp = new IPEndPoint (IPAddress.Any, 0);
+		}
+
+		public void ReceiveCommand()
+		{
+			
+		}
+
+
+	}
+}
+
