78b75b1bbcbac92ebd293affa870c4c1ab1f1a8b
Nikolai Topping <nikolai.topping@gmail.com>
2017-03-15 11:22:52 +0100

    DET HELE

--- File_Transfer/UPD/File_Client/File_Client.cs
+++ File_Transfer/UPD/File_Client/File_Client.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace File_Client
 {
 	public class File_Client
 	{
-		public File_Client ()
+	    private UdpClient _udpClient;
+	    private string _hostIp;
+		public File_Client (string[] args)
 		{
+		    _hostIp = args[0];
+		    _udpClient = new UdpClient(9000);
 		}
+
+	    public void SendCommand()
+	    {
+	        string cmdToSend = Console.ReadLine();
+            _udpClient.Connect(_hostIp,9000);
+	        Byte[] bytesToSend = Encoding.ASCII.GetBytes(cmdToSend);
+	        _udpClient.Send(bytesToSend, bytesToSend.Length);
+	    }
+
+	    public void ReceiveAnswer()
+	    {
+	        IPEndPoint remoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+	        Byte[] receivedBytes = _udpClient.Receive(ref remoteIpEndPoint);
+            string returnData = Encoding.ASCII.GetString(receivedBytes);
+	        Console.WriteLine(returnData);
+        }
+
+
 	}
 }
 
--- File_Transfer/UPD/File_Server/File_Server.cs
+++ File_Transfer/UPD/File_Server/File_Server.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace File_Server
 {
@@ -12,11 +14,32 @@ namespace File_Server
 		{
 			_udpServer = new UdpClient (portnumber);
 			_remoteIp = new IPEndPoint (IPAddress.Any, 0);
-		}
+            Console.WriteLine(">> Server has started.");
+        }
 
 		public void ReceiveCommand()
 		{
-			
+            Console.WriteLine(">> Waiting for new connection...");
+            Byte[] receivedBytes = _udpServer.Receive(ref _remoteIp);
+            string bytesAsString = Encoding.ASCII.GetString(receivedBytes);
+
+		    switch (bytesAsString)
+		    {
+                case "U":
+                case "u":
+                    FileStream fileReader = new FileStream("/proc/uptime", FileMode.Open);
+		            using (MemoryStream ms = new MemoryStream())
+		            {
+		                fileReader.CopyTo(ms);
+		                Byte[] buffer = ms.ToArray();
+		                _udpServer.Send(buffer, buffer.Length, _remoteIp);
+		            }
+                        break;
+
+                case "L":
+                case "l":
+		            break;
+		    }
 		}
 
 
